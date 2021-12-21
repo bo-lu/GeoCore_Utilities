604584275744cde75a80c44854e7332b470c0efe
Bo Lu <bo.lu@canada.ca>
2021-12-21 12:24:06 -0500

    Create dynamodb_obj.py
    A very simple DynamoDB wrapper written in python for AWS Lambda to handle general CRUD operations

---
+++ dyamodb_operations/dynamodb_obj.py
@@ -0,0 +1,108 @@
+import boto3
+
+from botocore.exceptions import ClientError
+
+""" A very simple DynamoDB wrapper written in python for AWS Lambda
+Handles general CRUD operations
+"""
+
+class Dynamodb:
+    def __init__(self, region):
+        #assuming aws secrets are know since we are running in lambda
+        self._region = region
+        self._dynamodb = boto3.resource('dynamodb', region_name=_region)
+        
+    def create_table (self, table_name, schema, attributes, throughput, gsi=None): 
+        """Create a table and wait for completion
+        """
+        try:
+            if(len(global_secondary_indexes)>0):
+                table = self._dynamodb.create_table(
+                    TableName=table_name,
+                    KeySchema=schema,
+                    AttributeDefinitions=attributes,
+                    GlobalSecondaryIndexes=gsi,
+                    ProvisionedThroughput=throughput
+                )
+            else:
+                table = self._dynamodb.create_table(
+                    TableName=table_name,
+                    KeySchema=schema,
+                    AttributeDefinitions=attributes,
+                    ProvisionedThroughput=throughput
+                )
+            #wait for table completion
+            table.meta.client.get_waiter('table_exists').wait(TableName=table)
+        except ClientError as e:
+            print(table, "table could NOT be created.")
+            return False
+        else:
+            return True
+
+    def create_item(self, table_name, item):
+        """Create an item (dict) in table_name
+        """
+        try:
+            table = self._dynamodb.Table(table_name)
+            response = table.put_item(Item=item)
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+        else:
+            return True
+    
+    def read_item(self, table_name, key):
+        """Read an item by key
+        """
+        try:
+            table = self._dynamodb.Table(table_name)
+            response = table.get_item(Key=key)
+            item = response.get('Item')
+            if item is not None:
+                return item
+            else:
+                return ItemNotFound(table_name, key)
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+            return False
+        else:
+            return True
+            
+    def query_item(self, table_name, expression, gsi=None):
+        """Query for item by expression and gsi
+        """
+        try:
+            table = self._dynamodb.Table(table_name)
+            if gsi != None:
+                response = table.query(
+                    IndexName=gsi,
+                    KeyConditionExpression=expression
+                )
+            else:
+                response = table.query(
+                    KeyConditionExpression=expression
+                )
+            return response.get('Items')
+        except ClientError as e:
+            print(e.response['Error']['Message'])
+            return False
+        else:
+            return True
+            
+    def update_item(self, table_name, key,
+                    attributes_to_update, operation="UPDATE_EXISTING_ATTRIBUTE_OR_ADD_NEW_ATTRIBUTE"):
+        pass
+    
+    def delete_table (self, table_name):
+        """Delete a table and wait for completion
+        """
+        try:
+            table = self._dynamodb.Table(table_name)
+            table.delete()
+            #wait for table delete completion
+            table.meta.client.get_waiter('table_not_exists').wait(TableName=table_name)
+        except ClientError as e:
+            print(table, "table could NOT be deleted:", e.response['Error']['Message'])
+            return False
+        else:
+            return True
+            
